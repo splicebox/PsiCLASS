40af262dac407855b4be7a9594e2fa66ecbb68ec
Li Song <lsong10@jhu.edu>
2017-09-18 14:50:55 -0400

    Add the wrapper script.

---
+++ run-classes.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/env perl
+
+use strict ;
+use warnings ;
+
+use Cwd 'cwd' ;
+use Cwd 'abs_path' ;
+use File::Basename;
+
+die "Usage: perl run-classes.pl [OPTIONS]\n".
+    "Required:\n".
+    "\t--lb STRING: the path to the file containing the alignments bam files\n".
+    "Optional:\n".
+    "\t-s STRING: the path to the trusted splice sites file (default: not used)\n".
+    "\t-p STRING: the prefix of output files (default: classes)\n". 
+    "\t--stage NUM:  (default: 0)\n".
+    "\t\t0-start from beginning (building splice sites for each sample)\n".
+    "\t\t1-start from building subexon files for each sample\n".
+    "\t\t2-start from combining subexon files\n".
+    "\t\t3-start from assembly the transcripts\n"
+    if ( @ARGV == 0 ) ;
+
+my $WD = dirname( abs_path( $0 ) ) ;
+
+my $i ;
+my $cmd ;
+my $prefix = "classes_" ;
+
+sub system_call
+{
+	print $_[0], "\n" ;
+	system( $_[0] ) ;
+}
+
+# Process the arguments
+my @bamFiles ;
+my $spliceFile = "" ;
+my $bamFileList ;
+for ( $i = 0 ; $i < @ARGV ; ++$i )
+{
+	if ( $ARGV[$i] eq "--lb" )
+	{
+		$bamFileList = $ARGV[$i + 1] ;
+		open FP1, $ARGV[$i + 1] ;	
+		while ( <FP1> )
+		{
+			chomp ;
+			push @bamFiles, $_ ;
+		}
+		++$i ;
+	}
+	elsif ( $ARGV[$i] eq "-s" )
+	{
+		$spliceFile = $ARGV[$i + 1] ;		
+		++$i ;
+	}
+	elsif ( $ARGV[$i] eq "-p" )
+	{
+		$prefix = $ARGV[$i + 1] ;
+		if ( substr( $prefix, -1 ) ne "_" )
+		{
+			$prefix .= "_" ;
+		}
+		++$i ;
+	}
+}
+if ( scalar( @bamFiles ) == 0 )
+{
+	die "Must use option --lb to specify the list of bam files.\n" ;
+}
+
+# Generate the splice file for each bam file.
+for ( $i = 0 ; $i < @bamFiles ; ++$i )
+{
+	system_call( "$WD/junc ".$bamFiles[$i]." -a > ${prefix}bam_$i.raw_splice" ) ;
+	if ( $spliceFile ne "" )
+	{
+		system_call( "perl $WD/ManipulateIntronFile.pl $spliceFile ${prefix}bam_$i.raw_splice > ${prefix}bam_$i.splice" ) ;
+	}
+	else
+	{
+		#system_call( "awk \'{if (\$6>1) print;}\' ${prefix}bam_$i.raw_splice > ${prefix}bam_$i.splice" ) ;
+		system_call( "mv ${prefix}bam_$i.raw_splice ${prefix}bam_$i.splice" ) ;
+	}
+}
+
+# Get subexons from each bam file
+open FPls, ">${prefix}subexon.list" ;
+for ( $i = 0 ; $i < @bamFiles ; ++$i )
+{
+	system_call( "$WD/subexon-info ".$bamFiles[$i]." ${prefix}bam_$i.splice > ${prefix}subexon_$i.out" ) ;	
+	print FPls "${prefix}subexon_$i.out\n" ;
+}
+
+# combine the subexons.
+$cmd = "$WD/combine-subexons --ls ${prefix}subexon.list > ${prefix}subexon_combined.out" ;
+system_call( "$cmd" ) ;
+
+# Run classes
+my $trimPrefix = substr( $prefix, 0, -1 ) ;
+$cmd = "$WD/classes --lb $bamFileList -s ${prefix}subexon_combine.out -o ${trimPrefix}" ;
+system_call( "$cmd" ) ;
